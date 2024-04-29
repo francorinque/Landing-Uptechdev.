0095c5e66088011febaed29b15544ea43eb486f5
francorinque <francoagustinrinquecorreo@gmail.com>
2024-04-29 14:25:38 -0300

    feat(SectionVideo): section SectionVideo was created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Employees from "./components/Employees/Employees";
 import Hero from "./components/Hero/Hero";
 import Industries from "./components/Industries/Industries";
 import Navbar from "./components/Navbar/Navbar";
+import VideoSection from "./components/VideoSection/VideoSection";
 import Layout from "./components/ui/Layout";
 
 function App() {
@@ -12,11 +13,14 @@ function App() {
         <section className='pt-[10px]'>
           <Hero />
         </section>
-        <section className='pt-[150px]'>
+        <section className='pt-[200px]'>
           <Industries />
         </section>
-        <section className='pt-[150px]'>
+        <section className='pt-[200px]'>
           <Employees />
+        </section>
+        <section className='pt-[150px] lg:pt-[250px]'>
+          <VideoSection />
         </section>
       </Layout>
     </main>

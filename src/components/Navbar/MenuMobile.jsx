6cfccd923c3b7eb0afd48270c22a12c5a7abdf32
francorinque <francoagustinrinquecorreo@gmail.com>
2024-05-01 10:51:40 -0300

    feat(Blog): add link to Blog

--- src/components/Navbar/MenuMobile.jsx
+++ src/components/Navbar/MenuMobile.jsx
@@ -17,11 +17,24 @@ const MenuMobile = ({ isOpen, onClick }) => {
         w-[250px] flex flex-col gap-4  items-start text-dark bg-nav shadow-md shadow-black/10
         `}
       >
-        {data["section-nav"].links.map(({ text, href }) => (
-          <li key={uuidv4()} className='p-2'>
-            <CustomLink text={text} href={href} animated />
-          </li>
-        ))}
+        {data["section-nav"].links.map(({ text, href, isExternal, title }) =>
+          isExternal ? (
+            <li key={uuidv4()} className='p-2'>
+              <CustomLink
+                text={text}
+                href={href}
+                animated
+                rel='noreferrer'
+                target='_blank'
+                title={title}
+              />
+            </li>
+          ) : (
+            <li key={uuidv4()} className='p-2'>
+              <CustomLink text={text} href={href} animated title={title} />
+            </li>
+          )
+        )}
       </ul>
     </div>
   );
